ade7950c23ab341f652aac76002c384d3024dfe7
cankskrk <ardacankisakurek@gmail.com>
2025-01-18 02:52:25 +0300

    feat: complete Home page implementation

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery
   } from 'react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 
@@ -18,5 +18,12 @@ export const useCreateUserAccount = () => {
 export const useSignInAccount = () => {
     return useMutation({
         mutationFn: (user: {email: string, password: string}) => signInAccount(user)
+    })
+}
+
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
     })
 }
